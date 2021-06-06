7e99a1df9817f7cca8d9b819874268a1e729c6e4
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-06-06 23:55:18 +0530

    pro2

--- jscrpit/gjs.js
+++ jscrpit/gjs.js
@@ -1,3 +1,40 @@
 "use strict";
-console.log(document.querySelector("h3").textContent);
-document.querySelector("h3").textContent = "heyyy";
+/*console.log(document.querySelector("h3").textContent);
+document.querySelector("h2").textContent = 20;
+
+document.querySelector(".guess").value = 14;
+console.log(document.querySelector(".guess").value);
+
+document.querySelector(".check").addEventListener("click", function () {
+  document.querySelector(".guess").value = 18;
+});
+*/
+let high = 0;
+const y = Math.trunc(Math.random() * 20) + 1;
+console.log(y);
+let x2 = 20;
+document.querySelector(".check").addEventListener("click", function () {
+  let x = Number(document.querySelector(".guess").value);
+  if (!x) {
+    document.querySelector(".idea").textContent = "Use a Number";
+  } else {
+    if (x > y) {
+      document.querySelector(".idea").textContent = "High";
+      x2--;
+      document.querySelector(".scoren").textContent = x2;
+    }
+    if (x < y) {
+      document.querySelector(".idea").textContent = "Low";
+      x2--;
+      document.querySelector(".scoren").textContent = x2;
+    }
+    if (x == y) {
+      document.querySelector(".idea").textContent = "Perfect!!";
+      document.querySelector("h2").textContent = y;
+      if (x2 > high) {
+        high = x2;
+        document.querySelector(".high-scoren").textContent = x2;
+      }
+    }
+  }
+});
